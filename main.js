73de2497cb79e3269b88e1dae30013a89d2dbdab
jimmyli <jyl702@gmail.com>
2011-11-08 18:35:28 -0500

    Finished movement functionality

--- main.js
+++ main.js
@@ -1,21 +1,49 @@
 $(document).ready(function() {
-
+	
+	//plug in pics into DOM
 	var pic_array = ["1.JPG", "2.JPG", "3.JPG", "4.JPG", "5.JPG", "6.JPG"];
-
+	
 	for(pic in pic_array) {
 	$("#canvas").append("<img src=\"images/" + pic_array[pic] + "\">");
 	}
+	
+	//create buttons in appropriate positions
+	var rows = 3;
+	var cols = 2;
+	var count = 0; 
+	
+	for(i=0; i<rows; i++){
+		for(j=0; j<cols; j++){
+			$('#thumbs').append("<a href=\"#\"><img class=\"row-" + i + " col-" + j + "\" src=\"images/" + pic_array[count] + "\" /></a>").hide();
+			count += 1
+		}
+	}
 
-	var window_width = $(window).width();
-	var window_height = $(window).height();
+	var button_width = 40;
+	var button_height = 30;
+	var button_container_width = button_width*cols;
+	var button_container_height = button_height*rows;
+	
+	$('#thumbs').children().children().css({
+		'width': button_width,
+		'height': button_height,
+		'display': 'inline-block',
+		'float': 'left'
+	});
+
+	$('#thumbs').css({
+		'width': button_container_width,
+		'height': button_container_height
+	}).show();
+	
+	//set width and movement variables
+	var pic_width = $(window).width()/2.5;
+	var pic_height = $(window).height()/2.5;
 	
 	var margin = 5;
 	
-	var rows = 3;
-	var columns = 2;
-	
-	var canvas_width = (columns * window_width) + (columns*2*margin);
-	var canvas_height = (rows * window_height) + (rows*2*margin);
+	var canvas_width = (cols * pic_width) + (cols*2*margin);
+	var canvas_height = (rows * pic_height) + (rows*2*margin);
 
 	$("#canvas").css({
 	"width": canvas_width,
@@ -23,62 +51,34 @@ $(document).ready(function() {
 	});
 
 	$("#canvas").children().css({
-	"width": window_width,
-	"height": window_height,
+	"width": pic_width,
+	"height": pic_height,
 	"margin": (margin + "px")
 	});
 	
-	var lr_mov = window_width+(margin*2);
-	var ud_mov = window_height+(margin*2); 
-	
-	
-	
-function move(dir, speed){
-	var $old_top = parseInt($('#canvas').css('top'));
-	var $old_left = parseInt($('#canvas').css('left'));
-	
-	if(dir === 'down') {		
-		$('#canvas').animate({ 
-			top: ($old_top - ud_mov) + 'px'
-		}, speed);
-	}
+	var lr_mov = pic_width+(margin*2);
+	var ud_mov = pic_height+(margin*2); 
 
-	else if(dir === 'up') {
-		$('#canvas').animate({ 
-			top: ($old_top + ud_mov) + 'px'
-		}, speed);
-	}
+//generic move function
+function move(end_row, end_col, speed){
+	var $new_top = end_row*pic_height;
+	var $new_left = end_col*pic_width;
+	
+	$('#canvas').animate({ 
+		top: -($new_top)
+	}, speed);
 
-	else if(dir === 'right') {
-		$('#canvas').animate({ 
-			left: ($old_left - lr_mov) + 'px'
-		}, speed);
-	}
-
-	else if(dir === 'left') {
-		$('#canvas').animate({ 
-			left: ($old_left + lr_mov) + 'px'
-		}, speed);
-	}
+	$('#canvas').animate({ 
+		left: -($new_left)
+	}, speed);
 }
 	
-	
-	$("#right_btn").click(function () {
-		move('right', 1000);
+	$("#thumbs").children().click(function () {
+		var class_name = $(this).children().attr('class');
+		row_num = class_name.charAt(4);
+		col_num = class_name.charAt(10);
+		move(row_num, col_num, 1200);
 	});
-
-	$("#left_btn").click(function () {
-		move('left', 1000);
-	});
-	
-	$("#down_btn").click(function () {
-		move('down', 1000);
-	});
-	
-	$("#up_btn").click(function () {
-		move('up', 1000);	
-	});
-
 
 });
 
