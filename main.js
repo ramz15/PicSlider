96d2a11831f3d8c96369d29b09e946fcd45cfc6c
jimmyli <jyl702@gmail.com>
2011-11-08 14:23:59 -0500

    Slide right works

--- main.js
+++ main.js
@@ -1,61 +1,40 @@
 $(document).ready(function() {
 
-  var pic_array = ["1.JPG", "2.JPG", "3.JPG", "4.JPG", "5.JPG"];
+	var pic_array = ["1.JPG", "2.JPG", "3.JPG", "4.JPG", "5.JPG", "6.JPG"];
 
-  for(pic in pic_array) {
-    $("#pics").append("<img src=\"images/" + pic_array[pic] + "\">");
-  }
+	for(pic in pic_array) {
+	$("#canvas").append("<img src=\"images/" + pic_array[pic] + "\">");
+	}
 
-  var window_width = $(window).width();
-  var pic_width = (pic_array.length * window_width + 50);
-  
-  $("#pics").css({
-    "width": pic_width
-  });
-  
-  $("#pics").children().css({
-    "width": window_width,
-    "height": "100%"
-  });
+	var window_width = $(window).width();
+	var window_height = $(window).height();
+	
+	var margin = 5;
+	
+	var rows = 3;
+	var columns = 2;
+	
+	var canvas_width = (columns * window_width) + (columns*2*margin);
+	var canvas_height = (rows * window_height) + (rows*2*margin);
 
-  $("IMG:not(:first-child)").hide();
+	$("#canvas").css({
+	"width": canvas_width,
+	"height": canvas_height
+	});
 
-  var $current_img = $("#pics").children().filter(":visible");
-
-  $("#scroll_btn").click(function () {
-    
-    // if($current_img.before()){
-    //   $current_img.before().hide();
-    // }
-    var $next_img = $current_img.next();
-    
-    $current_img.animate({ 
-      // improve to complete leave screen
-      left: -($current_img.width()+30)
-    }, 1000,function() {
-      $next_img.show();
-      $current_img.hide(function() {
-        $next_img.animate({
-          left: 0
-        });
-      });
-    });
-
-    
-    // $current_img.hide();
-  
-    
-    // $next_img.css({
-    //   "left": $next_img.width() + 30
-    // });
-    
-    // $current_img.hide();
-    //     
-    $current_img = $next_img;
-    
-  });
-  
+	$("#canvas").children().css({
+	"width": (window_width-(margin*2)),
+	"height": (window_height-(margin*2)),
+	"margin": (margin + "px")
+	});
+	
+	$("#scroll_btn").click(function () {
+		$('#canvas').animate({ 
+		  left: -(window_width+(margin*2))
+		});
+	});
 });
+
 
 
 
