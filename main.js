83b96d88b79d38d9c2b1e3771d1c11014c2d8c30
Karl Gusner <karlgusner@gmail.com>
2011-11-10 14:01:21 -0500

    added new coord_to_index function, changed thumb pic to pic

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 	var thumb_array = ["1_thumb.JPG", "2_thumb.JPG", "3_thumb.JPG", "4_thumb.JPG", "5_thumb.JPG", "6_thumb.JPG"];
 	
 	for(pic in pic_array) {
-	$("#canvas").append("<img src=\"images/" + thumb_array[pic] + "\">");
+	$("#canvas").append("<img id=\"" + pic + "\" src=\"images/" + thumb_array[pic] + "\">");
 	}
 	
 	//create buttons in appropriate positions
@@ -15,10 +15,12 @@ $(document).ready(function() {
 	
 	for(i=0; i<rows; i++){
 		for(j=0; j<cols; j++){
-			$('#thumbs').append("<a href=\"#\"><img class=\"row-" + i + " col-" + j + "\" src=\"images/" + thumb_array[count] + "\" /></a>").hide();
+			$('#thumbs').append("<a href=\"#\"><img class=\"row-" + i + " col-" + j + "\"  src=\"images/" + thumb_array[count] + "\" /></a>").hide();
 			count += 1
 		}
 	}
+	
+	
 
 	var button_width = 40;
 	var button_height = 30;
@@ -59,20 +61,37 @@ $(document).ready(function() {
 	
 	var lr_mov = pic_width+(margin*2);
 	var ud_mov = pic_height+(margin*2); 
-
-//generic move function
-function move(end_row, end_col, speed){
-	var $new_top = end_row*pic_height;
-	var $new_left = end_col*pic_width;
 	
-	$('#canvas').animate({ 
-		top: -($new_top)
-	}, speed);
+	function coord_to_index(pic_row, pic_col) {
+	  var count = 0;
+	  for (i=0; i<rows; i++) {
+	    for (j=0; j<cols; j++) {
+	      if (i===pic_row && j===pic_col)
+	        return count;
+	      else
+	        count++;
+	    }   
+	  } 
+	}
 
-	$('#canvas').animate({ 
-		left: -($new_left)
-	}, speed);
-}
+  //generic move function
+  function move(end_row, end_col, speed){
+  	var $new_top = end_row*pic_height;
+  	var $new_left = end_col*pic_width;
+	
+  	$('#canvas').animate({ 
+  		top: -($new_top)
+  	}, speed);
+
+  	$('#canvas').animate({ 
+  		left: -($new_left)
+  	}, speed);
+	
+  	var index = coord_to_index(parseInt(end_row, 10), parseInt(end_col, 10) );
+    $('#' + index).attr("src", "images/" + pic_array[index] );
+	  
+	  $('#' + index).attr("src", "images/" + thumb_array[index] );
+  }
 	
 	$("#thumbs").children().click(function () {
 		var class_name = $(this).children().attr('class');
