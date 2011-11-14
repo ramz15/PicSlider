5f3b24da2e015489da390e68eef4a3aa23f903d7
jimmyli <jyl702@gmail.com>
2011-11-14 17:38:36 -0500

    Improved gaussian blur

--- blurAndThumb.rb
+++ blurAndThumb.rb
@@ -1,23 +1,21 @@
 require 'RMagick'
 include Magick
 
-pic_temp = ["images/1.JPG"]
 pic_array = ["images/1.JPG", "images/2.JPG", "images/3.JPG", "images/4.JPG", "images/5.JPG", "images/6.JPG", "images/7.JPG", "images/8.JPG", "images/9.JPG"]
 
-# #generate thumbs
-# pic_array.each do |a| 
-#   image = ImageList.new(a)
-#   mini_image = image.scale(300, 199)
-#   match = a.match /\/(\d+)\./
-#   mini_image.write("images/" + match[1] + "_thumb.JPG")
-# end 
-
-#generate blurred images
-pic_temp.each do |a| 
+#generate thumbs
+pic_array.each do |a| 
   image = ImageList.new(a)
-  blur_image = image.gaussian_blur(0.0, 5.0)
-  blur_image.display
-end
-
-  
-
+  mini_image = image.scale(300, 199)
+  match = a.match /\/(\d+)\./
+  mini_image.write("images/" + match[1] + "_thumb.JPG")
+end 
+ 
+#generate blurred images
+pic_array.each do |a| 
+  image = ImageList.new(a)
+  #resize and blur
+  blur_image = image.resize(1500,996).gaussian_blur(15.0, 15.0)
+  match = a.match /\/(\d+)\./
+  blur_image.write("images/" + match[1] + "_blur.JPG") 
+end 
