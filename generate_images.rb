1359fcaf41f4fbe6b80fd661fbf4ab1e07c83b02
jimmyli <jyl702@gmail.com>
2011-11-15 15:06:03 -0500

    Automatically form pic_array in the ruby script

--- generate_images.rb
+++ generate_images.rb
@@ -1,16 +1,23 @@
 require 'RMagick'
 include Magick
 
-pic_array = ["images/1.JPG", "images/2.JPG", "images/3.JPG", "images/4.JPG", "images/5.JPG", "images/6.JPG", "images/7.JPG", "images/8.JPG", "images/9.JPG"]
+pic_array = []
+Dir.foreach('images/') do |item|
+  next if item[0] == '.' or item[0..1] == '..'
+  pic_array << ("images/" + item)
+end 
+ 
+# #output the total number of images in the folder 
+puts "You have " + (pic_array.length).to_s + " images"
 
-#generate standardized names for focused images
+# #generate standardized names for focused images
 pic_array.each do |a|
   image = ImageList.new(a)
   match = a.match /\/(\d+)\./
   image.write("images/" + match[1] + "_focused.JPG")
 end 
 
-# #generate thumbs
+#generate thumbs
 pic_array.each do |a| 
   image = ImageList.new(a)
   mini_image = image.scale(300, 199)
@@ -18,7 +25,7 @@ pic_array.each do |a|
   mini_image.write("images/" + match[1] + "_thumb.JPG")
 end
 
-# #generate blurred images
+#generate blurred images
 pic_array.each do |a| 
   image = ImageList.new(a)
   #resize and blur
